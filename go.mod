df1385ca4273004b3f96d6174e46ddf3c134e566
francisco.a.cunha <francisco.a.cunha@kuehne-nagel.com>
2025-07-05 01:18:03 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module system-info-mcp-server
+
+go 1.24
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"system-info-mcp-server/server"
+	"time"
+)
+
+func main() {
+	// This is the entry point of the application.
+	// You can initialize your application here.
+	fmt.Println("Hello World")
+
+	mcpServer, err := server.NewMCPSystemInfoServer("MCPServer-test", "1.0.0", 8080, []string{}, true, time.Now(), map[string]string{"configKey": "configValue"})
+	if err != nil {
+		fmt.Println("Error creating MCP server:", err)
+		return
+	}
+	fmt.Println(mcpServer.GetInfo())
+
+	errChangeName := mcpServer.SetName("MCPServer-Updated")
+	if errChangeName != nil {
+		fmt.Println("Error:", errChangeName)
+		return
+	}
+	fmt.Println(mcpServer.GetInfo())
+
+	if status, startTime, err := mcpServer.GetStatus(); err != nil {
+		fmt.Println("Error getting status:", err)
+		return
+	} else {
+		fmt.Printf("Server Status: %s, Start Time: %s\n", status, startTime)
+	}
+
+	if config, err := mcpServer.GetConfig(); err != nil {
+		fmt.Println("Error getting config:", err)
+	} else {
+		fmt.Println("Server Config:", config)
+	}
+
+	if err := mcpServer.Stop(); err != nil {
+		fmt.Println("Error stopping server:", err)
+	} else {
+		fmt.Println("Server stopped successfully")
+	}
+	if status, _, err := mcpServer.GetStatus(); err != nil {
+		fmt.Println("Error getting status:", err)
+		return
+	} else {
+		fmt.Printf("Server Status: %s\n", status)
+	}
+}
