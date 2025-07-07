07609ea7ac2d0eac4906ccd6de46013cc7dfe777
franciscocunha55 <afrancisco97@gmail.com>
2025-07-07 22:11:56 +0100

    add support to allow llm to get memory usage

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	//serverTest "github.com/franciscocunha55/mcp-server-training/serverTest"
 
 	"context"
@@ -21,15 +22,30 @@ func helloHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallTo
 
 
 
+func getSystemMemoryInfoHandler(ctx context.Context, request mcp.CallToolRequest)( *mcp.CallToolResult, error){
+	// systemInfo, err := request.RequireString("system information")
+	// if err != nil {
+	// 	return mcp.NewToolResultError(err.Error()), nil
+	// }
+	
+	var memory runtime.MemStats
+
+	// Passes the pointer so the function can modify the original variable
+	runtime.ReadMemStats(&memory)
+
+	return mcp.NewToolResultText(fmt.Sprintf("currently allocated bytes: %d!, total system memory: %d, number of garbage collections %d, number of objects on heap: %d", memory.Alloc, memory.Sys / 1024, memory.NumGC, memory.HeapObjects)), nil
+	
+}
+
 func main() {
 
 	serverMcp := server.NewMCPServer(
-		"Hello World",
+		"MCP server Golang",
 		"1.0.0",
 		server.WithToolCapabilities(false),
 	)
 
-	tool := mcp.NewTool("hello_world",
+	toolHelloWorld := mcp.NewTool("hello_world",
 		mcp.WithDescription("Say hello to someone"),
 		mcp.WithString("name",
 			mcp.Required(),
@@ -37,7 +53,14 @@ func main() {
 		),
 	)
 
-	serverMcp.AddTool(tool, helloHandler)
+	toolGetSystemInfo := mcp.NewTool("get_system_memory_info",
+		mcp.WithDescription("Get detailed system memory usage including allocated bytes, heap objects, and garbage collection stats"),
+,
+	)	
+
+	
+	serverMcp.AddTool(toolHelloWorld, helloHandler)
+	serverMcp.AddTool(toolGetSystemInfo, getSystemMemoryInfoHandler)
 
 	// Start the stdio server
 	if err := server.ServeStdio(serverMcp); err != nil {
